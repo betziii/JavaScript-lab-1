0c33ebfdfe64ed13caf2a6ea4797b21976efe261
Bethelhem Teshibelay <se.bethelhem.teshibelay@gmail.com>
2021-01-24 16:41:31 +0300

    calculator with 9 services is done

--- calculator/assets/js/calc.js
+++ calculator/assets/js/calc.js
@@ -3,50 +3,127 @@ var num1;
 var num2;
 var amount;
 let service;
-let result;
+var result = 0;
 
-service = prompt("do you want Addtion, Substraction, Multiplication or Division?");
-num1 = prompt("Enter your first number");
-num2 = prompt("Enter your second number");
-
+service = prompt("do you want Addtion, Substraction, Multiplication, Division, Maximum, Minimum, Average, Square or Square Root?");
 let serve = service.toLowerCase();
-if( serve === "substraction"){
-    function sub(num1,num2){
-        result = Number(num1) - Number(num2);
-    }
-}else{
-    if (serve === "division") {
+(function(){
+    if( serve === "substraction"){
+        num1 = prompt("Enter your first number");
+        num2 = prompt("Enter your second number");
+        sub(num1,num2);
+        function sub(num1,num2){
+            result = Number(num1) - Number(num2);
+            console.log("The result is "+ result);
+        }
+    }else if (serve === "division") {
+        num1 = prompt("Enter your first number");
+        num2 = prompt("Enter your second number");
         if (Number(num2)=== 0){
-            function div(num1,num2){
+            function dive(num1,num2){
                 result = Number(num1) / Number(num2);
+                console.log("The result is "+ result);
             }
+            dive(num1,num2);
+    
         }
         else{
             console.log("the denominator should not be zero.");
         }
-    } else {
-        if (serve === "addition") {
-            amount = prompt("Enter the amount of numbers?");
-            let arr = new Array(parseInt(amount));
-            for(i=0; i<arr.length; i++){
-                num = prompt("Enter the number");
-                arr.push(num);
-            }
-            function add(arr){
-                for (var i = 0; i < arr.length; i++) {
-                    console.log(arr[i]);
-                  }
-            }
-            
-        } else {
-            
-        }
+    }else if (serve === "square") {
+        numb = prompt("Enter your number");
+        (function square(){
+            result = Math.pow(numb,2);
+            console.log("The result is "+ result);
+        })(numb)
         
+    }else if (serve === "addition") {
+        amount = prompt("Enter the amount of numbers?");
+        let arr = new Array();
+        for(i=0; i<amount; i++){
+            num = prompt("Enter the number");
+            arr.push(num);
+        }
+        arr.forEach(add);
+        function add(item){
+            result += Number(item);
+        }
+        console.log("The result is "+ result);
+                
+    } else if(serve==="multiplication"){
+        amount = prompt("Enter the amount of numbers?");
+        let arr = new Array();
+        for(i=0; i<amount; i++){
+            num = prompt("Enter the number");
+            arr.push(num);
+        }
+        arr.forEach(add);
+        function add(item){
+            result *= Number(item);
+            }
+        console.log("The result is "+ result);
+    }else if(serve==="maximum"){
+        amount = prompt("Enter the amount of numbers?");
+        let arr = new Array();
+        for(i=0; i<amount; i++){
+            num = prompt("Enter the number");
+            arr.push(num);
+        }
+        (function maximum(){
+            let max = arr[0];
+            for (let i = 1; i < arr.length; ++i) {
+                if (arr[i] > max) {
+                  max = arr[i];
+                }
+              }
+              console.log("The result is "+ max);  
+        })(arr);
+        
+    }else if(serve==="minimum"){
+        amount = prompt("Enter the amount of numbers?");
+        let arr = new Array();
+        for(i=0; i<amount; i++){
+            num = prompt("Enter the number");
+            arr.push(num);
+        }
+        (function minimum(){
+            let min = arr[0];
+            for (let i = 1; i < arr.length; ++i) {
+                if (arr[i] < min) {
+                  min = arr[i];
+                }
+              }
+              console.log("The result is "+ min);  
+        })(arr);
+                
+    }else if(serve==="average"){
+        amount = prompt("Enter the amount of numbers?");
+        let arr = new Array();
+        for(i=0; i<amount; i++){
+            num = prompt("Enter the number");
+            arr.push(num);
+        }
+        (function average(){
+            var tot = 0;
+            for(var i = 0; i < arr.length; i++) {
+                tot += arr[i];
+            }
+            var avg = tot / arr.length;
+            console.log("The result is "+ avg);
+              })(arr);
+              
+    }else if (serve === "square root") {
+        numbe = prompt("Enter your number");
+        
+        if (Number(numbe)> 0){
+            (function squareroot(){
+                result = Math.sqrt(Number(numbe));
+                console.log("The result is "+ result);
+            })(numbe);
+        }
+        else{
+            console.log("The number should not be negative.");
+        }
     }
-}
-  
 
-
-
-
-
+})(serve);
